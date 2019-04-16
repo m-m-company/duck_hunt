6cc97131f714599b870f044e3398c2972186a560
MatteoLoria <mloria58@gmail.com>
2019-04-16 18:49:12 +0200

    shot

---
+++ core/src/com/duck_hunt/DuckHunt.java
@@ -0,0 +1,47 @@
+package com.duck_hunt;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class DuckHunt extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Sound sound;
+	Pixmap pm;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("back.png");
+		sound = Gdx.audio.newSound(Gdx.files.internal("shot.wav"));
+		pm = new Pixmap(Gdx.files.internal("cursor.png"));
+		Gdx.graphics.setCursor(Gdx.graphics.newCursor(pm, 0, 0));
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+			System.exit(0);
+		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+			sound.play();
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+		sound.dispose();
+		pm.dispose();
+	}
+}
---
+++ desktop/src/com/duck_hunt/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.duck_hunt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.duck_hunt.DuckHunt;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = true;
+		new LwjglApplication(new DuckHunt(), config);
+	}
+}
